2f0f1f72a1bc8df8ae9cb0ebaf9fe745d89ae8d7
hafizmuhammadsaadullah <saadullah20512gmail.com>
2020-11-02 14:33:00 +0500

    * Adds UI changes with bootstrap and devise controllers
    * Configured bootstrap for views
    * Change defalut layout for devise views
    * Added devise controllers only for redirection in certain methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  layout :writers_and_readers
+
+  private
+
+  def writers_and_readers
+    user_signed_in? ? 'application' : 'login'
+  end
 end
