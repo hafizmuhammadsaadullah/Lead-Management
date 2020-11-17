2fb718f70d80c8ab6d01c85e1aacd1944525233f
hafizmuhammadsaadullah <saadullah20512gmail.com>
2020-11-17 17:51:36 +0500

    change production configuration for email v4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,12 +74,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     domain: 'gmail.com',
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    ssl: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
