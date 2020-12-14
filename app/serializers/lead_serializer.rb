0cb7f535bc48a6825f6f0508f277cfcfe3063d16
hafizmuhammadsaadullah <saadullah20512gmail.com>
2020-12-14 16:39:08 +0500

    Code refactoring

--- app/serializers/lead_serializer.rb
+++ app/serializers/lead_serializer.rb
@@ -4,4 +4,3 @@ class LeadSerializer < ActiveModel::Serializer
   attributes :id, :name, :user_id, :client_name, :client_email, :client_contact, :client_address, :transition_date,
              :leadType, :is_sale, :created_at, :updated_at
 end
-cd ../
